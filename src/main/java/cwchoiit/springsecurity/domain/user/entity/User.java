1824e4ca862fbaaf08414d92f26abcb7bd323919
cwchoi <cwchoi@osci.kr>
2024-10-04 14:43:02 +0900

    UserDetails를 구현한 User 엔티티를 사용해서, Authorities에 해당하는 Role을 직접 구현
    기존에는, UserDetailsService를 구현하기 위해 구현할 메서드인 loadUserByUsername()에서 반환하는 타입이 스프링 시큐리티의 유저 객체였지만,
    직접 만든 User 객체로 UserDetails를 구현했기 때문에 이 User 객체를 그대로 사용할 수 있도록 Role ENUM을 생성하고 authorities를 대체

--- src/main/java/cwchoiit/springsecurity/domain/user/entity/User.java
+++ src/main/java/cwchoiit/springsecurity/domain/user/entity/User.java
@@ -2,13 +2,15 @@ package cwchoiit.springsecurity.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 @Table(name = "users") // H2 Database 예약어 때문에 user 라는 테이블을 만들 수 없음 따라서 users 테이블 명을 명시
 @Getter
@@ -21,21 +23,25 @@ public class User implements UserDetails {
     private Long id;
     private String username;
     private String password;
-    private String authority;
 
-    public User(String username, String password, String authority) {
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    public User(String username, String password, Role role) {
         this.username = username;
         this.password = password;
-        this.authority = authority;
+        this.role = role;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton((GrantedAuthority) () -> this.authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) () -> role.getRole());
+        return authorities;
     }
 
     public boolean isAdmin() {
-        return authority.equals("ROLE_ADMIN");
+        return this.role == Role.ROLE_ADMIN;
     }
 
     @Override
@@ -56,5 +62,21 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum Role {
+        ROLE_ADMIN("ROLE_ADMIN"),
+        ROLE_USER("ROLE_USER");
+
+        private final String role;
+
+        public static Role getRole(String role) {
+            return Arrays.stream(Role.values())
+                    .filter(r -> r.role.equals(role))
+                    .findFirst()
+                    .orElse(null);
+        }
     }
 }
