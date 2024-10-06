2a13731390bbeccde71b73697fb5c6f5b4187810
cwchoiit <chiwon99881@gmail.com>
2024-10-06 15:14:37 +0900

    User 관련 서비스 리팩토링

--- src/main/java/cwchoiit/springsecurity/domain/user/service/UserService.java
+++ src/main/java/cwchoiit/springsecurity/domain/user/service/UserService.java
@@ -4,7 +4,9 @@ import cwchoiit.springsecurity.domain.user.dto.UserRegisterDTO;
 import cwchoiit.springsecurity.domain.user.entity.User;
 
 public interface UserService {
-    void signup(UserRegisterDTO userRegisterDTO);
+    User signup(UserRegisterDTO userRegisterDTO);
 
     User signupAdmin(UserRegisterDTO userRegisterDTO);
+
+    User findByUsername(String username);
 }
