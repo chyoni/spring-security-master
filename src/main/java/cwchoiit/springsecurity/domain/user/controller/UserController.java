7f7aef43e1216bc0b0bace8113b461c8a02316d3
cwchoi <cwchoi@osci.kr>
2025-02-26 15:07:29 +0900

    REST API로 로그인 하는 경우엔 "/login" path 필요 없음

--- src/main/java/cwchoiit/springsecurity/domain/user/controller/UserController.java
+++ src/main/java/cwchoiit/springsecurity/domain/user/controller/UserController.java
@@ -16,11 +16,6 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/login")
-    public String login() {
-        return "/login";
-    }
-
     @GetMapping("/user/signup")
     public String signup() {
         return "/signup";
