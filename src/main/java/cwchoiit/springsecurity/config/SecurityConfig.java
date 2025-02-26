b6d20986386ab61b10ab871ddd146e545586abd4
cwchoi <cwchoi@osci.kr>
2025-02-26 11:04:37 +0900

    주석 추가 ++

--- src/main/java/cwchoiit/springsecurity/config/SecurityConfig.java
+++ src/main/java/cwchoiit/springsecurity/config/SecurityConfig.java
@@ -46,19 +46,20 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic authentication disabled
                 .csrf(AbstractHttpConfigurer::disable) // CSRF protection disabled
                 .rememberMe(AbstractHttpConfigurer::disable) // RememberMe authentication disabled
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // session authentication disabled
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session authentication disabled
+                )
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/home", "/h2-console/**", "/css/**", "/js/**", "/images/**", "/login", "/user/signup").permitAll()
+                        .requestMatchers(
+                                "/home", "/h2-console/**", "/css/**",
+                                "/js/**", "/images/**", "/login", "/user/signup"
+                        ).permitAll()
                         .requestMatchers("/note").hasRole("USER")
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // H2-Console iframe 정상 작동 (Origin 비교 후 같으면 그 iframe 요청은 허용)
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/home")
-                        .permitAll())
                 .logout(logout -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                         .logoutSuccessUrl("/login"));
--- src/main/java/cwchoiit/springsecurity/jwt/JwtAuthenticationFilter.java
+++ src/main/java/cwchoiit/springsecurity/jwt/JwtAuthenticationFilter.java
@@ -17,27 +17,31 @@ import java.io.IOException;
 import static cwchoiit.springsecurity.jwt.JwtProperties.*;
 
 /**
- * JWT Authentication Filter: JWT 를 사용해서 인증하는 필터
+ * Spring Security 에서 UsernamePasswordAuthenticationFilter 대신 사용될 JWT 를 이용한 인증 필터
+ * <p>
+ * 동작 방식은 다음 순서로 진행된다. <br>
+ * 1 - 이 필터를 Security Configuration 에 UsernamePasswordAuthenticationFilter 위치에 대신 등록한다. {@code addFilterAt(...)} <br>
+ * 2 - UsernamePasswordAuthenticationFilter 는 따로 사용자가 지정하지 않으면 기본으로 바라보고 있는 로그인 URL = POST["/login"] <br>
+ * 3 - 해당 경로로 인증 정보를 추가해서 요청하면 가장 먼저 이 필터의 {@code attemptAuthentication}가 실행된다. <br>
+ * 4 - 여기서, 사용자가 입력한 인증 정보를 바디 데이터로부터 읽어서 인증 시도를 한다. <br>
+ * 5 - 내부적으로 {@code ProviderManager.authenticate()} 메서드를 호출하고,
+ * {@code AbstractUserDetailsAuthenticationProvider.authenticate()} 메서드를 호출한다. 이 메서드 안에는 {@code retrieveUser()} 메서드가 호출되는데,
+ * 그 메서드는 {@code DaoAuthenticationProvider}의 {@code retrieveUser()}이고 이 메서드에서 바로 {@code UserDetailsService.loadUserByUsername()}메서드를 호출한다. <br>
+ * 6 - {@code loadUserByUsername()}을 호출하면 내가 {@link cwchoiit.springsecurity.domain.user.service.impl.UserServiceImpl} 에서 재정의한 메서드가 호출된다. <br>
+ * 7 - {@code loadUserByUsername()}가 정상적으로 {@link org.springframework.security.core.userdetails.User}를 반환하면
+ * 이 필터의 {@code successfulAuthentication}를 호출한다. 여기서 로그인 성공 처리 로직을 작성하면 된다. <br>
+ * 8 - {@code loadUserByUsername()}가 정상적으로 반환되지 않으면 이 필터의 {@code unsuccessfulAuthentication}을 호출한다. <br>
  */
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
+    // setFilterProcessesUrl(...)로 로그인 처리 URL 따로 지정하지 않으면, 기본값은 POST, "/login" (UsernamePasswordAuthenticationFilter 내부에 지정)
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         super(authenticationManager);
         this.authenticationManager = authenticationManager;
     }
 
-    /**
-     * Spring Security 설정 클래스에서 지정한 로그인 URL("/login")에서 POST 요청을 받아 유저를 확인한다.
-     * <br>
-     * 내부동작은 다음과 같다. <br>
-     * 유저를 확인할 땐 내부적으로 Spring Security 가 사용하는 {@code UserDetailsService.loadUserByUsername()} 메서드를 호출한다.
-     * 그래서 실제로 해당 유저가 있어야하고 {@code UserDetailsService} 를 구현한 구현체가 {@code loadUserByUsername()} 메서드를 구현해야 한다.
-     * 그게 {@code UsernamePasswordAuthenticationFilter} 에서 유저를 인증하는 방식이다. 내부적으로 {@code ProviderManager.authenticate()} 메서드를 호출하고,
-     * {@code AbstractUserDetailsAuthenticationProvider.authenticate()} 메서드를 호출한다. 이 메서드 안에는 {@code retrieveUser()} 메서드가 호출되는데,
-     * 그 메서드는 {@code DaoAuthenticationProvider} 의 {@code retrieveUser()} 이고 이 메서드에서 바로 {@code UserDetailsService.loadUserByUsername()} 메서드를 호출한다.
-     */
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         String username = request.getParameter("username");
